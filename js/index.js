880122df68d1c222f0526c6863e0d87e1b207047
francomurina <fmurina@udesa.edu.ar>
2020-11-11 16:25:08 -0300

    Cambios varios

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-fetch('https://developers.themoviedb.org/')
+fetch('https://api.themoviedb.org/3/movie/550?api_key=3801289076602860794bddb717c8f4f5')
 .then(function(response){
     return response.json();
 })
