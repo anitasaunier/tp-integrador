8cd4d7fe74e1cafa941446aa9a846a22a8ee7f8d
Ana Saunier <anasaunier@MacBook-Air-de-Ana.local>
2020-11-12 13:48:49 -0300

    cambios

---
+++ home/js/script.js
@@ -0,0 +1,22 @@
+ 
+fetch('https://api.themoviedb.org/3/movie/550?api_key=3801289076602860794bddb717c8f4f5')
+.then(function(response){
+    return response.json();
+})
+.then(function(data){
+    console.log(data)
+    let item = document.querySelector('.item')
+    console.log(item)
+      
+  let movie = ` <li class="item" >
+  <a href="../detalle/movieDetail.html"><img class="img-ambos" src="img/elhoyo.jpeg" alt="elhoyo" ></a>
+  <h2 class= "title">${data.title}</h2>
+                </li>`
+                
+               
+                
+                
+})
+.catch(function(error){
+    console.log('El error fue: ' + error)
+})
