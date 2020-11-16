8da770c2b546d71934886acf28befd4483f0643a
Ana Saunier <anasaunier@air-de-ana.lan>
2020-11-16 15:45:04 -0300

    cambios

--- home/js/script.js
+++ home/js/script.js
@@ -1,25 +1,12 @@
- 
-fetch('https://api.themoviedb.org/3/movie/550?api_key=3801289076602860794bddb717c8f4f5')
-.then(function(response){
-    return response.json();
-})
-.then(function(data){
-    console.log(data)
-    let item = document.querySelector('.item')
-    console.log(item)
-      
-  let movie = ` <li class="item" >
-  <a href="../detalle/movieDetail.html"><img class="img-ambos" src="img/elhoyo.jpeg" alt="elhoyo" ></a>
-  <h2 class= "title">${data.title}</h2>
-                </li>`
-                
-               
-                
-                
-})
-.catch(function(error){
-    console.log('El error fue: ' + error)
-})
+fetch('https://api.themoviedb.org/3/trending/all/day?api_key=3801289076602860794bddb717c8f4f5')
+.then(function(respuestas){    return respuestas.json()})
+.then(function(data){    
+    console.log(data)    
+    let info = data.results    
+    let tiras =document.querySelector('');       
+     for( let i=0; i<info.length; i++){        
+         tiras.innerHTML += 
+         .catch(function(error){    console.log(error);})
 
 
 
