24a1308f0d9918e1797f892d8ab4991bd9b627fa
francomurina <fmurina@udesa.edu.ar>
2020-11-17 11:06:52 -0300

    Acomodo y subo la parte de generos

--- generos/indexg.js
+++ generos/indexg.js
@@ -12,22 +12,31 @@ fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=3801289076602860794
     let info = data.genres
     // capturar nombre y id
     let generos = document.querySelector('.dgeneros');
-    
+    console.log(generos)
     for(let i=0; i<info.length; i++){
         generos.innerHTML +=`
-        <div class="separadores"><h2 class="titulofuturo"></h2>${info[i].name}</div>
-        <section class="contenidos">
-        <article class="card-wrapper">
-            <div class="img-contenido">
-                <h5 class="card-title"> ${info[i].id}</h5>
-                <a href="../detalle/movieDetail.html"><img class="img-ambos" src="img/presenciadelmal.jpeg" alt="presenciadelmal"></a>
-            </div>
-        </article>
-        </section>`
+        <div class="separadores"><h2 class="titulo">${info[i].name}</h2></div>
+        <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slider>
+        
+            <ul class=" lista-peliculas uk-slider-items ">
+            </ul>
+            <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
+            <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slider-item="next"></a>
+        
+        
+        </div>`
 
     }
 })
 .catch(function(error){
     console.log(error);
 })
-
+// <div class="separadores"><h2 class="titulofuturo"></h2>${info[i].name}</div>
+// <section class="contenidos">
+// <article class="card-wrapper">
+//     <div class="img-contenido">
+//         <h5 class="card-title"> ${info[i].id}</h5>
+//         <a href="../detalle/movieDetail.html"><img class="img-ambos" src="img/presenciadelmal.jpeg" alt="presenciadelmal"></a>
+//     </div>
+// </article>
+// </section>
