4723d4e84ca61941836259289c209bdcf6f18e85
Ana Saunier <anasaunier@air-de-ana.lan>
2020-11-16 22:54:17 -0300

    cambios

--- home/js/script.js
+++ home/js/script.js
@@ -1,13 +1,22 @@
 fetch('https://api.themoviedb.org/3/trending/all/day?api_key=3801289076602860794bddb717c8f4f5')
-.then(function(respuestas){    return respuestas.json()})
+.then(function(respuestas){    
+    return respuestas.json()
+})
 .then(function(data){    
     console.log(data)    
     let info = data.results    
-    let tiras =document.querySelector('');       
+    let lista =document.querySelector('.img-ambos');       
      for( let i=0; i<info.length; i++){        
-         tiras.innerHTML += 
-         .catch(function(error){    console.log(error);})
+         lista.innerHTML += `
+                             <img src=https://image.tmdb.org/t/p/w500${info[i].poster_path}                  `
 
 
 
-  
+
+.catch(function(error){    
+             console.log(error);
+            })
+
+
+
+   
